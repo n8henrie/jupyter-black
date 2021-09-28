2e258b250de5e5ba9a46c25fb90166d457972712
Nathan Henrie <nate@n8henrie.com>
2021-09-28 08:50:51 -0600

    Update to v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # [Changelog](https://keepachangelog.com)
 
+## 0.1.1 :: 2021-09-28
+
+- Unload the proper event
+
 ## 0.1.0 :: 2021-09-28
 
 - First release on PyPI.
--- src/jupyter_black/__init__.py
+++ src/jupyter_black/__init__.py
@@ -1,5 +1,5 @@
 """Reexport used modules, main code will be in `jupyter_black.py`."""
-__version__ = "v0.1.0"
+__version__ = "v0.1.1"
 __author__ = "Nathan Henrie"
 __email__ = "nate@n8henrie.com"
 
